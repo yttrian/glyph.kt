0f5bf2ad56386e595554b8137b45f6ff01ecdd8e
IanM-56 <privet.privet250@gmail.com>
2018-01-21 02:03:10 -0500

    Add checks to Skill class

--- src/main/java/me/ianmooreis/glyph/orchestrators/SkillOrchestrator.kt
+++ src/main/java/me/ianmooreis/glyph/orchestrators/SkillOrchestrator.kt
@@ -1,6 +1,7 @@
 package me.ianmooreis.glyph.orchestrators
 
 import ai.api.model.AIResponse
+import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import org.slf4j.Logger
 import org.slf4j.simple.SimpleLoggerFactory
@@ -16,20 +17,32 @@ object SkillOrchestrator {
     }
 
     fun trigger(event: MessageReceivedEvent, ai: AIResponse) {
-        println(this.skills)
         val result = ai.result
         val action = result.action
         val skill: Skill? = this.skills[action]
         if (skill != null) {
-            skill.onTrigger(event, ai)
+            skill.trigger(event, ai)
         } else {
             event.message.reply(if (result.fulfillment.speech.isEmpty()) "`$action` is not available yet!" else result.fulfillment.speech)
         }
     }
 }
 
-abstract class Skill(val trigger: String) {
-    open fun onTrigger(event: MessageReceivedEvent, ai: AIResponse) {
+abstract class Skill(val trigger: String, private val serverOnly: Boolean = false, private val requiredPermissions: Collection<Permission> = emptyList()) {
+    val log : Logger = SimpleLoggerFactory().getLogger(this.javaClass.simpleName)
 
+    open fun onTrigger(event: MessageReceivedEvent, ai: AIResponse) {
+        this.log.warn("This skill does nothing!")
+    }
+
+    fun trigger(event: MessageReceivedEvent, ai: AIResponse) {
+        val permitted: Boolean = event.member?.hasPermission(requiredPermissions) ?: true
+        if ((serverOnly || requiredPermissions.isNotEmpty()) && !event.channel.type.isGuild ) {
+            event.message.reply("You can only do this in a server!")
+        } else if (!permitted) {
+            event.message.reply("You don't have the required permissions to do that! (${requiredPermissions.joinToString { it.name }})")
+        } else {
+            this.onTrigger(event, ai)
+        }
     }
 }

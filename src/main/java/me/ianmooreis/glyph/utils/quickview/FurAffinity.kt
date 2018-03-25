9c45252484ce8b16910adc9dcdab6049404ddd4f
IanM-56 <privet.privet250@gmail.com>
2018-03-24 20:36:43 -0400

    Add support for story type submissions

--- src/main/java/me/ianmooreis/glyph/utils/quickview/FurAffinity.kt
+++ src/main/java/me/ianmooreis/glyph/utils/quickview/FurAffinity.kt
@@ -18,21 +18,23 @@ import java.util.*
 
 class Submission(private val title: String, private val description: String, private val name: String, private val profile: URL, val link: URL,
                  val posted: String, private val posted_at: Date, private val download: URL, private val full: URL, private val thumbnail: URL,
-                 private val category: String, private val theme: String, private val species: String, private val gender: String,
-                 private val favorites: Int, private val comments: Int, private val views: Int, private val resolution: String, val rating: SubmissionRating,
+                 private val category: String, private val theme: String, private val species: String?, private val gender: String?,
+                 private val favorites: Int, private val comments: Int, private val views: Int, private val resolution: String?, val rating: SubmissionRating,
                  private val keywords: List<String>) {
     fun getEmbed(thumbnail: Boolean): MessageEmbed {
         val linkedKeywords = keywords.joinToString { "[$it](https://www.furaffinity.net/search/@keywords%20$it)" }
         val fancyKeywords = if (linkedKeywords.length < 1024) linkedKeywords else keywords.joinToString()
+        val fileType = download.toString().substringAfterLast(".")
+        print(species+gender+download+resolution)
         return EmbedBuilder()
                 .setTitle(title, link.toString())
                 .setThumbnail(if (thumbnail) full.toString() else null)
                 .setDescription(
-                        "**Category** $category > $theme\n" +
-                        "**Species** $species\n" +
-                        "**Gender** $gender\n" +
+                        "**Category** $category > $theme (${rating.name})\n" +
+                        (if (species != null) "**Species** $species\n" else "") +
+                        (if (gender != null) "**Gender** $gender\n" else "") +
                         "**Favorites** $favorites | **Comments** $comments | **Views** $views" +
-                        if ((thumbnail && rating.nsfw) || !rating.nsfw) "\n**Download** [$resolution]($download)" else "")
+                        if ((thumbnail && rating.nsfw) || !rating.nsfw) "\n**Download** [${resolution ?: fileType}]($download)" else "")
                 .addField("Keywords", fancyKeywords, false)
                 .setFooter("FurAffinity", null)
                 .setColor(rating.color)

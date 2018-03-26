90b17b8bbe320e5860d0a85995ac95004ee53a8f
IanM-56 <privet.privet250@gmail.com>
2018-03-25 20:09:10 -0400

    Prevent duplicate starboarding
    Kinda basic, but check to make sure there is only one reaction and
    add one ourselves to prevent the original user from spamming it

--- src/main/java/me/ianmooreis/glyph/orchestrators/StarboardOrchestrator.kt
+++ src/main/java/me/ianmooreis/glyph/orchestrators/StarboardOrchestrator.kt
@@ -4,7 +4,7 @@ import com.vdurmont.emoji.EmojiParser
 import me.ianmooreis.glyph.extensions.asPlainMention
 import me.ianmooreis.glyph.extensions.config
 import net.dv8tion.jda.core.EmbedBuilder
-import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent
+import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 import org.slf4j.Logger
 import org.slf4j.simple.SimpleLoggerFactory
@@ -14,26 +14,38 @@ import java.time.Instant
 object StarboardOrchestrator : ListenerAdapter() {
     private val log : Logger = SimpleLoggerFactory().getLogger(this.javaClass.simpleName)
 
-    override fun onMessageReactionAdd(event: MessageReactionAddEvent) {
+    override fun onGuildMessageReactionAdd(event: GuildMessageReactionAddEvent) {
         val starboardConfig = event.guild.config.starboard
-        val emojiName = EmojiParser.parseToAliases(event.reactionEmote.name).removeSurrounding(":")
-        if (starboardConfig.enabled && emojiName == starboardConfig.emoji && starboardConfig.webhook != null && event.channelType.isGuild) {
+        val emojiName = emojiAlias(event.reactionEmote.name)
+        if (starboardConfig.enabled && emojiName == starboardConfig.emoji && starboardConfig.webhook != null) {
             event.channel.getMessageById(event.messageId).queue { message ->
-                val embed = EmbedBuilder().setAuthor(message.author.asPlainMention, null, message.author.avatarUrl)
-                    .setDescription(message.contentDisplay)
-                    .setImage(message.attachments.getOrNull(0)?.url ?: message.embeds.getOrNull(0)?.image?.url)
-                    .setThumbnail(message.embeds.getOrNull(0)?.thumbnail?.url)
-                    .setFooter("Starboard | ${message.id} in #${message.textChannel.name}", null)
-                    .setColor(Color.YELLOW)
-                    .setTimestamp(Instant.now())
-                message.embeds.forEach {
-                    val value = ((it.description ?: "") + it.fields.joinToString("") { "\n**__${it.name}__**\n${it.value}" })
-                    embed.addField(it.title ?: it.author.name,
-                            if (value.length < 1024) value else "${value.substring(0..1020)}...",
-                            false)
+                val duplicate = (message.reactions.findLast { emojiAlias(it.reactionEmote.name) == starboardConfig.emoji }?.count ?: 0) > 1
+                if (!duplicate) {
+                    when (event.reactionEmote.emote) {
+                        null -> message.addReaction(event.reactionEmote.name).queue()
+                        else -> message.addReaction(event.reactionEmote.emote).queue()
+                    }
+                    val embed = EmbedBuilder().setAuthor(message.author.asPlainMention, null, message.author.avatarUrl)
+                            .setDescription(message.contentDisplay)
+                            .setImage(message.attachments.getOrNull(0)?.url ?: message.embeds.getOrNull(0)?.image?.url)
+                            .setThumbnail(message.embeds.getOrNull(0)?.thumbnail?.url)
+                            .setFooter("Starboard | ${message.id} in #${message.textChannel.name}", null)
+                            .setColor(Color.YELLOW)
+                            .setTimestamp(Instant.now())
+                    message.embeds.forEach {
+                        val value = ((it.description
+                                ?: "") + it.fields.joinToString("") { "\n**__${it.name}__**\n${it.value}" })
+                        embed.addField(it.title ?: it.author.name,
+                                if (value.length < 1024) value else "${value.substring(0..1020)}...",
+                                false)
+                    }
+                    WebhookOrchestrator.send(event.jda.selfUser, starboardConfig.webhook, embed.build())
                 }
-                WebhookOrchestrator.send(event.jda.selfUser, starboardConfig.webhook, embed.build())
             }
         }
+    }
+
+    private fun emojiAlias(emoji: String): String {
+        return EmojiParser.parseToAliases(emoji).removeSurrounding(":")
     }
 }

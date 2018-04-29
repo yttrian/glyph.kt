2402e3ab6514e216b2d41cfc508e9a3d597347e1
IanM-56 <privet.privet250@gmail.com>
2018-04-29 18:45:26 -0400

    Handle invalid snowflakes gracefully

--- src/main/java/me/ianmooreis/glyph/skills/SnowstampSkill.kt
+++ src/main/java/me/ianmooreis/glyph/skills/SnowstampSkill.kt
@@ -2,6 +2,7 @@ package me.ianmooreis.glyph.skills
 
 import ai.api.model.AIResponse
 import me.ianmooreis.glyph.extensions.reply
+import me.ianmooreis.glyph.orchestrators.CustomEmote
 import me.ianmooreis.glyph.orchestrators.SkillAdapter
 import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
@@ -10,10 +11,16 @@ import java.awt.Color
 
 object SnowstampSkill : SkillAdapter("skill.snowstamp") {
     override fun onTrigger(event: MessageReceivedEvent, ai: AIResponse) {
-        val snowflake = ai.result.getStringParameter("snowflake").toLong()
-        val snowflakeInstant = MiscUtil.getCreationTime(snowflake).toInstant()
+        val snowflake = ai.result.getStringParameter("snowflake")
+        val snowflakeId = try {
+            snowflake.toLong()
+        } catch (e: NumberFormatException) {
+            event.message.reply("${CustomEmote.XMARK} `$snowflake` is not a snowflake!")
+            return
+        }
+        val snowflakeInstant = MiscUtil.getCreationTime(snowflakeId).toInstant()
         event.message.reply(EmbedBuilder()
-                .setTitle(snowflake.toString())
+                .setTitle(snowflakeId.toString())
                 .setDescription("**UTC** $snowflakeInstant\n**UNIX** ${snowflakeInstant.toEpochMilli()}")
                 .setColor(Color.WHITE)
                 .setFooter("Snowstamp", null)

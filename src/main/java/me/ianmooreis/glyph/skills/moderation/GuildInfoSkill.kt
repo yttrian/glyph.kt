05337e7f28e8710bac68e50bb8d55500ca0d40de
IanM-56 <privet.privet250@gmail.com>
2018-04-30 17:09:57 -0400

    Add ability to ask for specific properties of a guild

--- src/main/java/me/ianmooreis/glyph/skills/moderation/GuildInfoSkill.kt
+++ src/main/java/me/ianmooreis/glyph/skills/moderation/GuildInfoSkill.kt
@@ -1,13 +1,34 @@
 package me.ianmooreis.glyph.skills.moderation
 
 import ai.api.model.AIResponse
-import me.ianmooreis.glyph.extensions.getInfoEmbed
-import me.ianmooreis.glyph.extensions.reply
+import me.ianmooreis.glyph.extensions.*
 import me.ianmooreis.glyph.orchestrators.SkillAdapter
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import org.ocpsoft.prettytime.PrettyTime
 
 object GuildInfoSkill : SkillAdapter("skill.moderation.guildInfo", guildOnly = true) {
     override fun onTrigger(event: MessageReceivedEvent, ai: AIResponse) {
-        event.message.reply(event.guild.getInfoEmbed("Guild Info", "Moderation", null, true))
+        val property: String? = ai.result.getStringParameter("guildProperty", null)
+        if (property != null) {
+            val guild = event.guild
+            event.message.reply(when (property) {
+                "name" -> "This guild is **${guild.name}**."
+                "id" -> "The id for ${guild.name} is **${guild.id}**."
+                "region" -> "${guild.name} is located in **${guild.regionRaw}**."
+                "created" -> "${guild.name} was created **${PrettyTime().format(guild.creationTime.toDate())}** (${guild.creationTime})."
+                "owner" -> "**${guild.owner.asPlainMention}** is the owner of ${guild.name}."
+                "members" -> "${guild.name} has **${guild.members.count()}** members."
+                "membersHumans" -> "${guild.name} has **${guild.members.count { !it.user.isBot }}** humans."
+                "membersBots" -> "${guild.name} has **${guild.members.count { it.user.isBot }}** bots."
+                "channels" -> "${guild.name} has **${guild.textChannels.size + guild.voiceChannels.size}** channels."
+                "channelsText" -> "${guild.name} has **${guild.textChannels.size}** text channels."
+                "channelsVoice" -> "${guild.name} has **${guild.voiceChannels.size}** voice channels."
+                "roles" -> "${guild.name} has **${guild.roles.size}** roles."
+                "farm" -> if (guild.isBotFarm) "**Yes**, ${guild.name} is a bot farm!" else "**No**, ${guild.name} is not a bot farm."
+                else -> "I'm not sure what property `$property` is for a guild."
+            })
+        } else {
+            event.message.reply(event.guild.getInfoEmbed("Guild Info", "Moderation", null, true))
+        }
     }
 }

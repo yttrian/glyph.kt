0cb088ff59228f34260595e81696ccc3d75ea18f
Ian Moore <privet.privet250@gmail.com>
2018-10-25 03:24:21 -0400

    Fix starboard not working with the newest version of JDA

--- src/main/java/me/ianmooreis/glyph/directors/StarboardDirector.kt
+++ src/main/java/me/ianmooreis/glyph/directors/StarboardDirector.kt
@@ -43,25 +43,33 @@ object StarboardDirector : ListenerAdapter() {
     override fun onGuildMessageReactionAdd(event: GuildMessageReactionAddEvent) {
         val starboardConfig = event.guild.config.starboard
         val emojiName = emojiAlias(event.reactionEmote.name)
+
+        // Welcome to callback hell
+        // TODO: Make these starboard handling/checks "shallower"
         if (starboardConfig.enabled && emojiName == starboardConfig.emoji && starboardConfig.webhook != null) {
             event.channel.getMessageById(event.messageId).queue { message ->
-                //Prevent self-starring if disallowed
+                // Prevent self-starring if disallowed
                 if (message.author == event.user && !starboardConfig.allowSelfStarring && message.author != event.jda.selfUser) {
                     event.reaction.removeReaction(event.user).queue()
                     return@queue
                 }
-                //Check whether the message should be sent to the starboard, with no duplicates and not a starboard of a starboard
-                val thresholdMet = (message.reactions.findLast { emojiAlias(it.reactionEmote.name) == starboardConfig.emoji }?.count
-                    ?: 0) >= starboardConfig.threshold
-                val isStarboarded = message.reactions.findLast { emojiAlias(it.reactionEmote.name) == starboardConfig.emoji }?.users?.contains(event.jda.selfUser)
-                    ?: false
-                val isStarboard = if (message.isWebhookMessage && message.embeds.size > 0) message.embeds[0].footer?.text?.contains("Starboard")
-                    ?: false else false
-                if (thresholdMet && !isStarboarded && !isStarboard) {
-                    //Mark the message as starboarded and send it to the starboard
-                    when (event.reactionEmote.emote) {
-                        null -> message.addReaction(event.reactionEmote.name).queue { sendToStarboard(message, event.jda.selfUser, starboardConfig.webhook) }
-                        else -> message.addReaction(event.reactionEmote.emote).queue { sendToStarboard(message, event.jda.selfUser, starboardConfig.webhook) }
+                // Check whether the message should be sent to the starboard, with no duplicates and not a starboard of a starboard
+                val starboardReactions = message.reactions.findLast { emojiAlias(it.reactionEmote.name) == starboardConfig.emoji }
+                    ?: return@queue
+                val thresholdMet = (starboardReactions.count) >= starboardConfig.threshold
+                // We have to make a separate request to actually see who reacted, this is new
+                starboardReactions.users.queue { reactedUsers ->
+                    if (!reactedUsers.contains(event.jda.selfUser)) {
+                        val messageFooter = message.embeds.getOrNull(0)?.footer?.text ?: ""
+                        val isStarboard = (message.isWebhookMessage && message.embeds.size > 0 && messageFooter.contains("Starboard"))
+                        val send = { sendToStarboard(message, event.jda.selfUser, starboardConfig.webhook) }
+                        if (thresholdMet && !isStarboard) {
+                            // Mark the message as starboarded and send it to the starboard
+                            when (event.reactionEmote.emote) {
+                                null -> message.addReaction(event.reactionEmote.name).queue { send() }
+                                else -> message.addReaction(event.reactionEmote.emote).queue { send() }
+                            }
+                        }
                     }
                 }
             }
@@ -71,7 +79,7 @@ object StarboardDirector : ListenerAdapter() {
     private fun sendToStarboard(message: Message, selfUser: SelfUser, webhook: String) {
         val firstEmbed = message.embeds.getOrNull(0)
         //Set-up the base embed
-        val embed = EmbedBuilder().setAuthor(message.author.asPlainMention, null, message.author.avatarUrl)
+        val embed = EmbedBuilder().setAuthor(message.author.asPlainMention, message.jumpUrl, message.author.avatarUrl)
             .setDescription(message.contentRaw)
             .setFooter("Starboard | ${message.id} in #${message.textChannel.name}", null)
             .setColor(Color.YELLOW)

9aa4f625e7508d93a7e6b480c21fed034834d11b
Ian Moore <privet.privet250@gmail.com>
2020-01-29 22:35:53 -0500

    Pull status messages from a config file instead of being hardcoded

--- src/directors/StatusDirector.kt
+++ src/directors/StatusDirector.kt
@@ -23,10 +23,11 @@
 
 package me.ianmooreis.glyph.directors
 
-import net.dv8tion.jda.core.JDA
-import net.dv8tion.jda.core.OnlineStatus
-import net.dv8tion.jda.core.entities.Game
-import net.dv8tion.jda.core.events.ReadyEvent
+import com.google.gson.JsonParser
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.ReadyEvent
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -34,24 +35,17 @@ import kotlin.concurrent.schedule
  * Manages the status messages of the client
  */
 object StatusDirector : Director() {
-    private val playing = listOf(
-        "Quasar", "Armax Arsenal Arena", "Alliance Corsair", "Towers of Hanoi", "Shattered Eezo",
-        "Relay Defense", "Kepesh-Yakshi", "Firebreathing Thresher Maws of Doom", "Galaxy of Fantasy",
-        "Third Coil", "Chess", "Nine Men's Morris", "with the space hamster"
-    ).map { Game.playing(it) }
-    private val watching = listOf(
-        "Armax Arsenal Arena", "ships take off", "ships dock", "Fleet and Flotilla", "Requiem for a Reaper!",
-        "Blasto: the Jellyfish strings", "Blasto 6: Partners in Crime", "Blasto 7: Blasto Goes to War!",
-        "Blasto 8: Blasto Cures the Genophage!", "Elcor Hamlet", "Starless", "Blue's Anatomy", "Call Me Sally",
-        "Damaged: The Truth Behind the Citadel Crisis", "Exiles: Portraits of the Lost Quarians",
-        "Last of the Legion", "Goda Tavetara: Lulea Reborn", "Saren: A Hero Betrayed", "The Demon and the Nightmare",
-        "The Path of Lies: A History of the Alliance Military", "the extranet"
-    ).map { Game.watching(it) }
-    private val listening = listOf(
-        "skycars zoom by", "Expel 10", "Varrencage", "the hum of the ship", "the hum of a mass relay",
-        "the silence of space", "diplomats"
-    ).map { Game.listening(it) }
-    private val statuses: List<Game> = playing + watching + listening
+    private val statuses: List<Activity> by lazy {
+        val configFile = this.javaClass.classLoader.getResourceAsStream("activities.json")?.reader()
+        val config = JsonParser().parse(configFile).asJsonObject
+        val types = enumValues<Activity.ActivityType>()
+
+        types.flatMap { type ->
+            config.getAsJsonArray(type.name.toLowerCase()).map {
+                Activity.of(type, it.asString)
+            }
+        }
+    }
 
     /**
      * When the client is ready
@@ -69,11 +63,9 @@ object StatusDirector : Director() {
     /**
      * Change the presence of the client
      */
-    fun setPresence(jda: JDA, status: OnlineStatus = jda.presence.status, game: Game = jda.presence.game) {
+    fun setPresence(jda: JDA, status: OnlineStatus = jda.presence.status, game: Activity? = jda.presence.activity) {
         jda.presence.setPresence(status, game)
     }
 
-    private fun getRandomStatus(): Game {
-        return statuses[Random().nextInt(statuses.size)]
-    }
+    private fun getRandomStatus(): Activity = statuses.random()
 }

fdce14491e85f0fe3cef6ede30cd81bfdb4bf145
Ian Moore <privet.privet250@gmail.com>
2021-06-06 03:42:36 -0400

    Restore some details for NSFW submissions in SFW contexts

--- bot/src/main/kotlin/me/ianmooreis/glyph/bot/messaging/quickview/furaffinity/Submission.kt
+++ bot/src/main/kotlin/me/ianmooreis/glyph/bot/messaging/quickview/furaffinity/Submission.kt
@@ -116,41 +116,41 @@ data class Submission(
      */
     fun getEmbed(nsfwAllowed: Boolean, thumbnailAllowed: Boolean): MessageEmbed {
         val embed = EmbedBuilder()
-            .setFooter("FurAffinity")
-            .setColor(rating.color)
             .setAuthor(name, profile, avatar)
+            .setTitle(title, link)
+            .setColor(rating.color)
+            .setFooter("FurAffinity")
             .setTimestamp(Instant.parse(postedAt))
 
+        val description = SimpleDescriptionBuilder()
+
+        // Add the different fields to the quickview embed description
+        description.addField("Category", "$category - $theme (${rating.name})")
+        species?.let { description.addField("Species", it) }
+        gender?.let { description.addField("Gender", it) }
+        description.addField(null, "**Favorites** $favorites | **Comments** $comments | **Views** $views")
+
         if (rating.nsfw && !nsfwAllowed) {
-            embed.setDescription("Submissions with a rating of $rating cannot be previewed outside of a NSFW channel!")
+            val warningText = "Submissions with a rating of $rating cannot be previewed outside of a NSFW channel!"
+            embed.addField("Warning", warningText, false)
         } else {
+            if (thumbnailAllowed) {
+                embed.setImage(full)
+            }
+
+            val fileType = download.substringAfterLast(".")
+            description.addField("Download", "[${resolution ?: fileType}]($download)")
+
             val linkedKeywords = keywords.joinToString { "[$it](https://www.furaffinity.net/search/@keywords%20$it)" }
             val fancyKeywords = if (linkedKeywords.length < MessageEmbed.VALUE_MAX_LENGTH) {
                 linkedKeywords
             } else {
                 keywords.joinToString(limit = MessageEmbed.VALUE_MAX_LENGTH)
             }
-            val fileType = download.substringAfterLast(".")
-            val description = SimpleDescriptionBuilder()
 
-            // Add the different fields to the quickview embed description
-            description.addField("Category", "$category - $theme (${rating.name})")
-            species?.let { description.addField("Species", it) }
-            gender?.let { description.addField("Gender", it) }
-            description.addField(null, "**Favorites** $favorites | **Comments** $comments | **Views** $views")
-            description.addField("Download", "[${resolution ?: fileType}]($download)")
-
-            if (thumbnailAllowed) {
-                embed.setImage(full)
-            }
-
-            embed
-                .setTitle(title, link)
-                .setDescription(description.build())
-                .addField("Keywords", fancyKeywords, false)
-                .build()
+            embed.addField("Keywords", fancyKeywords, false)
         }
 
-        return embed.build()
+        return embed.setDescription(description.build()).build()
     }
 }

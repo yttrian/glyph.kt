e3b0eb83d59e81e800a270d1bf0d42c69b15c823
Ian Moore <privet.privet250@gmail.com>
2021-07-09 23:11:35 -0400

    Set up HOCON

--- glyph-bot/build.gradle.kts
+++ glyph-bot/build.gradle.kts
@@ -1,3 +1,27 @@
+/*
+ * build.gradle.kts
+ *
+ * Glyph, a Discord bot that uses natural language instead of commands
+ * powered by DialogFlow and Kotlin
+ *
+ * Copyright (C) 2017-2021 by Ian Moore
+ *
+ * This file is part of Glyph.
+ *
+ * Glyph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import tanvd.kosogor.proxy.shadowJar
 
 /*
@@ -63,4 +87,5 @@ dependencies {
     implementation("com.vdurmont:emoji-java:4.0.0")
     implementation("net.jodah:expiringmap:0.5.9")
     implementation("commons-codec:commons-codec:1.14")
+    implementation("com.typesafe:config:1.4.1")
 }

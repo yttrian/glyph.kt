0853c382ab2671d59500ffbb4bd049afd5780453
Ian Moore <privet.privet250@gmail.com>
2021-09-28 04:51:29 -0400

    Internalize help contents, and dynamically add owner information

--- glyph-bot/src/main/kotlin/skills/util/HelpSkill.kt
+++ glyph-bot/src/main/kotlin/skills/util/HelpSkill.kt
@@ -30,9 +30,12 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.interactions.components.Button
 import net.dv8tion.jda.api.interactions.components.ButtonStyle
+import org.yttr.glyph.bot.Glyph
 import org.yttr.glyph.bot.ai.AIResponse
+import org.yttr.glyph.bot.extensions.asPlainMention
 import org.yttr.glyph.bot.messaging.Response
 import org.yttr.glyph.bot.skills.Skill
+import org.yttr.glyph.shared.readMarkdown
 import java.awt.Color
 
 /**
@@ -41,9 +44,10 @@ import java.awt.Color
 class HelpSkill : Skill("skill.help") {
     override suspend fun onTrigger(event: MessageReceivedEvent, ai: AIResponse): Response {
         val name = event.jda.selfUser.name
+        val creator = event.jda.retrieveUserById(Glyph.conf.getLong("management.creator-id")).await()
         val embed = EmbedBuilder()
             .setTitle("$name Help")
-            .setDescription(ai.result.fulfillment.speech)
+            .setDescription(helpTemplate.format(name, creator.asPlainMention))
             .setColor(embedColor)
             .build()
 
@@ -51,6 +55,7 @@ class HelpSkill : Skill("skill.help") {
     }
 
     companion object {
+        private val helpTemplate = this::class.java.classLoader.readMarkdown("help.md") ?: "There is no help."
         private val embedColor = Color.decode("#4687E5")
 
         private fun linkButton(url: String, label: String, emoji: String) =
---
+++ glyph-bot/src/main/resources/help.md
@@ -0,0 +1,14 @@
+Hi, I'm **%s**, an experimental Discord bot created by %s.
+
+I use **machine learning** to process **natural language** requests you give to me to the best of my current trained
+ability.
+
+To see what I can do, be sure to check out my **full skills list** and **suggest new ones** you'd like to see, in the
+official server or via the anonymous feedback skill.
+
+[Skills List](https://gl.yttr.org/en/latest/skills.html) -
+[Configure](https://conf.gl.yttr.org/) -
+[Help Server](https://gl.yttr.org/server) -
+[Invite to Server](https://gl.yttr.org/invite) -
+[Sponsor](https://gl.yttr.org/sponsor) -
+[Source Code](https://gl.yttr.org/source)

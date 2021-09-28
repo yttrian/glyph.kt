a848dad1527f4327b26efec07afdb35bd9dc8e77
Ian Moore <privet.privet250@gmail.com>
2021-09-28 04:56:31 -0400

    Only add trailing space to continuations
    On desktop a leading training space is trimmed, but not on mobile.

--- glyph-shared/src/main/kotlin/ClassLoaderExt.kt
+++ glyph-shared/src/main/kotlin/ClassLoaderExt.kt
@@ -28,6 +28,6 @@ package org.yttr.glyph.shared
  * Read a Markdown file from the resources, treating single linebreaks as continuations
  */
 fun ClassLoader.readMarkdown(resourceFileName: String): String? =
-    this.getResourceAsStream(resourceFileName)?.bufferedReader()?.readLines()?.joinToString(" ") {
-        if (it.isBlank()) "\n\n" else it.trim()
+    this.getResourceAsStream(resourceFileName)?.bufferedReader()?.readLines()?.joinToString("") {
+        if (it.isBlank()) "\n\n" else (it.trim() + " ")
     }

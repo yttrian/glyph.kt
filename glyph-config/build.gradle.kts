edec9e10c98415f3d6578d75925b22838eee6608
Ian Moore <privet.privet250@gmail.com>
2021-12-14 17:31:11 -0500

    Update logback to version 1.2.8
    https://jira.qos.ch/browse/LOGBACK-1591

--- glyph-config/build.gradle.kts
+++ glyph-config/build.gradle.kts
@@ -1,3 +1,27 @@
+/*
+ * build.gradle.kts
+ *
+ * Glyph, a Discord bot that uses natural language instead of commands
+ * powered by DialogFlow and Kotlin
+ *
+ * Copyright (C) 2017-2021 by Ian Moore
+ *
+ * This file is part of Glyph.
+ *
+ * Glyph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import tanvd.kosogor.proxy.shadowJar
 
 /*
@@ -27,7 +51,6 @@ import tanvd.kosogor.proxy.shadowJar
 group = "org.yttr.glyph.config"
 version = "1.0"
 
-internal val logbackVersion: String by project.extra
 internal val ktorVersion: String by project.extra
 
 shadowJar {
@@ -43,7 +66,6 @@ tasks.named("stage") {
 
 dependencies {
     implementation(project(":glyph-shared"))
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("io.ktor:ktor-serialization:$ktorVersion")

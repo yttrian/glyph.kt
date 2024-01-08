a45c8d4063ce801de1d71339c3e0dde645a58902
Ian Moore <privet.privet250@gmail.com>
2024-01-07 22:06:00 -0500

    Re-license under MIT (#82)

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#  conf.py
-#
-#  Glyph, a Discord bot that uses natural language instead of commands
-#  powered by DialogFlow and Kotlin
-#
-#  Copyright (C) 2017-2022 by Ian Moore
-#
-#  This file is part of Glyph.
-#
-#  Glyph is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of
-#  the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 #
 # Glyph documentation build configuration file, created by
 # sphinx-quickstart on Mon Jun  5 16:24:08 2017.
--- glyph-shared/src/main/kotlin/pubsub/redis/Redlock.kt
+++ glyph-shared/src/main/kotlin/pubsub/redis/Redlock.kt
@@ -1,27 +1,3 @@
-/*
- * Redlock.kt
- *
- * Glyph, a Discord bot that uses natural language instead of commands
- * powered by DialogFlow and Kotlin
- *
- * Copyright (C) 2017-2021 by Ian Moore
- *
- * This file is part of Glyph.
- *
- * Glyph is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package org.yttr.glyph.shared.redis
 
 import io.lettuce.core.ScriptOutputType

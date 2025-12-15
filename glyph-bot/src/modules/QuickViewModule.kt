54935675ae6f37654ec456e870920c2fe8ce59dc
Ian Moore <privet.privet250@gmail.com>
2025-12-14 22:52:32 -0500

    Add volatile message support to the QuickView module

--- glyph-bot/src/modules/QuickViewModule.kt
+++ glyph-bot/src/modules/QuickViewModule.kt
@@ -3,25 +3,43 @@ package org.yttr.glyph.bot.modules
 import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.events.listener
 import dev.minn.jda.ktx.util.SLF4J
+import io.lettuce.core.ExperimentalLettuceCoroutinesApi
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.fold
 import kotlinx.coroutines.flow.take
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.exceptions.PermissionException
 import org.yttr.glyph.bot.data.ConfigStore
+import org.yttr.glyph.bot.data.RedisCoroutines
 import org.yttr.glyph.bot.data.ServerConfig
 import org.yttr.glyph.bot.quickview.furaffinity.FurAffinityGenerator
 
-class QuickViewModule(private val configStore: ConfigStore) : Module {
+@OptIn(ExperimentalLettuceCoroutinesApi::class)
+class QuickViewModule(
+    private val redis: RedisCoroutines,
+    private val configStore: ConfigStore,
+    private val generators: List<FurAffinityGenerator> = listOf(FurAffinityGenerator)
+) : Module {
     private val log by SLF4J
-    private val generators = listOf(FurAffinityGenerator)
 
     override fun boot(jda: JDA) {
+        // Try to make QuickViews for any human message
         jda.listener<MessageReceivedEvent> { event ->
             if (!event.author.isBot && !event.author.isSystem && !event.isWebhookMessage) {
                 makeQuickViews(event)
+            }
+        }
+
+        // Delete replies when the original message is deleted
+        jda.listener<MessageDeleteEvent> { event ->
+            if (event.isFromGuild) {
+                redis.getdel("Glyph:QuickView:${event.messageId}")?.let { replyId ->
+                    event.channel.deleteMessageById(replyId).queue()
+                }
             }
         }
     }
@@ -32,17 +50,20 @@ class QuickViewModule(private val configStore: ConfigStore) : Module {
         quickViews.take(count = EMBED_LIMIT).fold(initial = event.message) { message, newEmbed ->
             if (message == event.message) {
                 message.trySuppressEmbeds()
-                event.message.replyEmbeds(newEmbed).mentionRepliedUser(false).await()
-                // TODO: Readd volatile support
+                event.message.replyEmbeds(newEmbed).mentionRepliedUser(false).await().also { reply ->
+                    redis.set("Glyph:QuickView:${message.id}", reply.id)
+                }
             } else {
                 message.editMessageEmbeds(message.embeds + newEmbed).await()
             }
         }
     }
 
-    private fun Message.trySuppressEmbeds() {
-        suppressEmbeds(true).queue(null) { exception ->
-            log.debug("Unable to suppress embeds for QuickViews in $channelId", exception)
+    private suspend fun Message.trySuppressEmbeds() {
+        try {
+            suppressEmbeds(true).await()
+        } catch (e: PermissionException) {
+            log.debug("Unable to suppress embeds for QuickViews in $channelId", e)
         }
     }
 
